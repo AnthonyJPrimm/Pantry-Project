a65b163a4b76e645b713b6597f3f654cae9b8327
Anthony <149452206+AnthonyJPrimm@users.noreply.github.com>
2024-08-04 12:13:55 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, query, getDocs, doc, setDoc, deleteDoc, getDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCaMuRVECnCzK7XELrrLUfr0TJXLylRTp0",
-  authDomain: "hai-pantry.firebaseapp.com",
-  projectId: "hai-pantry",
-  storageBucket: "hai-pantry.appspot.com",
-  messagingSenderId: "802407385162",
-  appId: "1:802407385162:web:1a641e81e522d3373b7eb3",
-  measurementId: "G-JLXBHR6H8M"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
